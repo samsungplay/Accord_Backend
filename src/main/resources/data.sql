7ae833d307ea59e18b5bacb53958f1560c90bb55
parkmin <parkmin@parkminui-MacBookAir.local>
2025-02-07 18:44:54 -0600

    add if not exists check on index creation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pg_bigm;
-CREATE INDEX idx_chatroom_name ON accord_chatroom USING GIN(name gin_bigm_ops);
-CREATE INDEX idx_chatrecord_message ON chat_record USING GIN(message gin_bigm_ops);
-CREATE INDEX idx_chatrecord_attachments ON chat_record USING GIN(attachments gin_bigm_ops);
-CREATE INDEX idx_user_nickname ON accord_user USING GIN(nickname gin_bigm_ops);
-CREATE INDEX idx_user_username ON accord_user USING GIN(username gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_chatroom_name ON accord_chatroom USING GIN(name gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_chatrecord_message ON chat_record USING GIN(message gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_chatrecord_attachments ON chat_record USING GIN(attachments gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_user_nickname ON accord_user USING GIN(nickname gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_user_username ON accord_user USING GIN(username gin_bigm_ops);
 
